44b4473b9ce1b67dfae2b27be9ffa19d9a100398
HASHIR MUHAMMED S <iamhashir7@gmail.com>
2023-11-13 21:53:38 +0530

    netflix

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Row from './components/Row';
+import requests from './requests';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+function App() {
+
+  return (
+    <div>
+      <Nav/>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row isPoster={true} title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending in INDIA" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
